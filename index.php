f11055e89f58ffbae4492032af5a4359b3a3cd7a
Leandro de Souza <leandrosf26@gmail.com>
2018-11-08 01:31:42 -0200

    tela de login

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php 
 
+session_start();
+
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -20,10 +23,40 @@ $app->get('/', function() {				// Executa o código quando estiver na raiz do pr
 
 $app->get('/admin', function() {				// Executa o código quando estiver na raiz do projeto
     
+	User::verifyLogin();
+
 	$page = new PageAdmin();					// Método construtor da classe Page adiciona o header da página
 
 	$page->setTpl("index");				// Adiciona o conteúdo da página do site
 
+});
+
+
+$app->get('/admin/login', function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+});
+
+
+$app->post('/admin/login', function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+});
+
+$app->get('/admin/logout', function(){
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 });
 
 $app->run();							// Roda o template

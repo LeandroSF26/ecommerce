24c0b0a0d3f28bd83b332a303500fc7fb4def2fb
Leandro de Souza <leandrosf26@gmail.com>
2018-11-20 19:08:08 -0200

    Detalhes do produto

--- views-cache/categories-menu.6201bc10a0631ed8a29d1450ec6b99db.rtpl.php
+++ views-cache/categories-menu.6201bc10a0631ed8a29d1450ec6b99db.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/3">Android </a></li><li><a href="/categories/1">Apple</a></li><li><a href="/categories/2">Google</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/3">Android </a></li><li><a href="/categories/1">Apple</a></li><li><a href="/categories/4">Motorola</a></li>

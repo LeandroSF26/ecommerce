cf1db0e4608cf0210b67cae1ddd16a9fd2f67f54
Leandro de Souza <leandrosf26@gmail.com>
2018-11-25 22:39:00 -0200

    Esqueceu senha - site

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -187,7 +187,7 @@ class User extends Model{
 
 	}
 
-	public static function getForgot($email){
+	public static function getForgot($email, $inadmin = true){
 
 		$sql = new Sql();
 
@@ -220,7 +220,16 @@ class User extends Model{
 
 			//$code = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, User::SECRET, $dataRecovery["idrecovery"], MCRYPT_MODE_ECB));
 
-			$link = "http://www.ecommerce.com.br/admin/forgot/reset?code=$code";
+			if($inadmin === true){
+
+				$link = "http://www.ecommerce.com.br/admin/forgot/reset?code=$code";
+
+			}
+			else{
+				$link = "http://www.ecommerce.com.br/forgot/reset?code=$code";
+			}
+
+			
 
 			$mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir Senha da Hcode Store", "forgot", array(
 				"name"=>$data["desperson"],
